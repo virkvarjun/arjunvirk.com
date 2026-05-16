bf031fc2b6e6721251393d5c1e19b74b8bf36e3c
Arjun V <virkvarjun@gmail.com>
2026-05-16 16:30:35 -0700

    Add profile data module

---
+++ src/lib/data.ts
@@ -0,0 +1,12 @@
+export const profile = {
+  name: "Arjun Virk",
+  tagline: "software engineering @ university of waterloo",
+  bio: "I build things at the intersection of systems and product. Currently studying at the University of Waterloo.",
+  location: "Waterloo, ON",
+  socials: {
+    github: "https://github.com/arjunvirk",
+    linkedin: "https://linkedin.com/in/arjunvirk",
+    twitter: "https://x.com/arjunvirk",
+    email: "a23virk@uwaterloo.ca",
+  },
+};
